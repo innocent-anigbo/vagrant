d4cd937caaad8d78c3c09ac448571a6e4b6abf7d
Innocent Anigbo <innocentanigbo@Innocents-MacBook-Pro.local>
2018-01-29 16:27:06 +0000

    added kops file

---
+++ files/kops.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+wget https://github.com/kubernetes/kops/releases/download/1.7.0/kops-linux-amd64
+chmod +x kops-linux-amd64
+mv kops-linux-amd64 kops && mv kops /usr/bin/
+wget https://storage.googleapis.com/kubernetes-release/release/v1.7.3/bin/linux/amd64/kubectl
+chmod +x kubectl && mv kubectl /usr/bin/
+
+yum update -y
+apt-get install python-pip -y
+pip install awscli
