97544a1a196e5aa3e0bee9e1a7c37a54e34323b6
Innocent Anigbo <innocentanigbo@Innocents-MacBook-Pro.local>
2018-01-29 17:02:16 +0000

    test

--- files/kops.sh
+++ files/kops.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-wget https://github.com/kubernetes/kops/releases/download/1.7.0/kops-linux-amd64
+wget --quiet https://github.com/kubernetes/kops/releases/download/1.7.0/kops-linux-amd64
 chmod +x kops-linux-amd64
 mv kops-linux-amd64 kops && mv kops /usr/bin/
-wget https://storage.googleapis.com/kubernetes-release/release/v1.7.3/bin/linux/amd64/kubectl
+wget --quiet https://storage.googleapis.com/kubernetes-release/release/v1.7.3/bin/linux/amd64/kubectl
 chmod +x kubectl && mv kubectl /usr/bin/
 
 apt-get update -y
